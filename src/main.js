5a9e5472befd7a9991fd2a92e65bad722b8658f4
Murk <mrmurk4g3@outlook.com>
2020-02-17 23:45:01 +0000

    project files...

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from 'vue-router'
+
+import Dashboard from '@/components/Dashboard'
+import DashboardHome from '@/pages/Home'
+
+import store from './store'
+
+import '@/assets/css/tailwind.css'
+
 
 Vue.config.productionTip = false
 
+Vue.use(Router)
+
+const routes = [
+  { path: '/', redirect: { name: 'DashboardHome' } },
+  { path: '/dashboard', component: Dashboard, children: [
+      { path: '/', redirect: { name: 'DashboardHome' } },
+      { path: 'home', name: 'DashboardHome', component: DashboardHome }
+    ]
+  }
+]
+
+const router = new Router({
+  mode: 'history',
+  routes
+})
+
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
+
